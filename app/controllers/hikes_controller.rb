d33ac4aee2c9460638f4ef560fcf9ec44577c2a6
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-06-02 13:21:03 -0700

    added and tested hikes get route. Seeded the data from the seed file

--- app/controllers/hikes_controller.rb
+++ app/controllers/hikes_controller.rb
@@ -1,3 +1,8 @@
-class HikesController < ApplicationController
+class HikeController < ApplicationController
+
+    get "/hikes" do
+        @hikes = Hike.all 
+        erb :"hikes/index.html"
+    end
 
 end
