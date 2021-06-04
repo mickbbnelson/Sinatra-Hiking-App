ec730950ffa8172ad44396a6ecfecce6938ae64e
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-06-04 10:56:37 -0700

    Added user post route and user validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,13 @@ class UserController < ApplicationController
     get "/sign_up" do
         erb :"/users/sign_up.html"
     end
+
+    post "/sign_up" do
+        user = User.new(params)
+        if !user.save
+            redirect to "/sign_up"
+        else
+            redirect to "/hikes"  #need to update
+        end
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
     has_many :hikes
     has_secure_password 
+    validates :name, :username, :email_address, :password, presence: true
 end
