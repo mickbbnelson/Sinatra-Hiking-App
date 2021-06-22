4bc148c27051d67dffba6aca6096f385d7e0196c
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-06-22 15:32:23 -0700

    removed personal notes

--- config/environment.rb
+++ config/environment.rb
@@ -1,13 +1,13 @@
-ENV['SINATRA_ENV'] ||= "development"    #If the sinatra key is set as something, it will use that value, if not, it will set it as development.
+ENV['SINATRA_ENV'] ||= "development" 
 
 require 'bundler/setup'
-Bundler.require(:default, ENV['SINATRA_ENV'])   #requiring default with whatever version it is
+Bundler.require(:default, ENV['SINATRA_ENV'])   
 require 'dotenv/load'
 
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)                                               #Makes our code more flexible while establishing connection.
+)                                               
 
-require './app/controllers/application_controller'  #requires App Controller
-require_all 'app'                                   #requires the entire App directory
+require './app/controllers/application_controller'  
+require_all 'app'                                   
